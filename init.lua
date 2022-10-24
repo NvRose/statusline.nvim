d25afe61611897b782186fcc3d8abd01ffe198a9
fenze <contact@fenze.dev>
2022-10-25 00:50:46 +0200

    Fix formatting

--- init.lua
+++ init.lua
@@ -4,43 +4,43 @@ local cmd = vim.cmd
 local opt = vim.opt
 
 local modes = {
-  	["n"]    = { "NORMAL",   "NormalMode" },
-  	["no"]   = { "NORMAL",   "NormalMode" },
-  	["niI"]  = { "NORMAL",   "NormalMode" },
-  	["niR"]  = { "NORMAL",   "NormalMode" },
-  	["niV"]  = { "NORMAL",   "NormalMode" },
+	["n"] = { "NORMAL", "NormalMode" },
+	["no"] = { "NORMAL", "NormalMode" },
+	["niI"] = { "NORMAL", "NormalMode" },
+	["niR"] = { "NORMAL", "NormalMode" },
+	["niV"] = { "NORMAL", "NormalMode" },
 
-	["v"]    = { "VISUAL",   "VisualMode" },
-	["V"]    = { "VISUAL",   "VisualMode" },
-	[""]   = { "VISUAL",   "VisualMode" },
+	["v"] = { "VISUAL", "VisualMode" },
+	["V"] = { "VISUAL", "VisualMode" },
+	[""] = { "VISUAL", "VisualMode" },
 
-	["s"]    = { "SELECT",   "SelectMode" },
-	["S"]    = { "SELECT",   "SelectMode" },
-	[""]   = { "SELECT",   "SelectMode" },
+	["s"] = { "SELECT", "SelectMode" },
+	["S"] = { "SELECT", "SelectMode" },
+	[""] = { "SELECT", "SelectMode" },
 
-  	["i"]    = { "INSERT",   "InsertMode" },
-  	["ic"]   = { "INSERT",   "InsertMode" },
-  	["ix"]   = { "INSERT",   "InsertMode" },
+	["i"] = { "INSERT", "InsertMode" },
+	["ic"] = { "INSERT", "InsertMode" },
+	["ix"] = { "INSERT", "InsertMode" },
 
-  	["R"]    = { "REPLACE",  "ReplaceMode" },
-  	["Rv"]   = { "REPLACE",  "ReplaceMode" },
+	["R"] = { "REPLACE", "ReplaceMode" },
+	["Rv"] = { "REPLACE", "ReplaceMode" },
 
-	["c"]    = { "COMMAND",  "CmdMode"    },
-	["cv"]   = { "COMMAND",  "CmdMode"    },
-	["ce"]   = { "COMMAND",  "CmdMode"    },
+	["c"] = { "COMMAND", "CmdMode" },
+	["cv"] = { "COMMAND", "CmdMode" },
+	["ce"] = { "COMMAND", "CmdMode" },
 
-	["r"]    = { "PROMPT",   "PromptMode" },
-	["rm"]   = { "PROMPT",   "PromptMode" },
-	["r?"]   = { "PROMPT",   "PromptMode" },
+	["r"] = { "PROMPT", "PromptMode" },
+	["rm"] = { "PROMPT", "PromptMode" },
+	["r?"] = { "PROMPT", "PromptMode" },
 
-	["!"]    = { "TERMINAL", "TermMode"   },
-	["t"]    = { "TERMINAL", "TermMode"   },
-	["nt"]   = { "TERMINAL", "TermMode"   },
+	["!"] = { "TERMINAL", "TermMode" },
+	["t"] = { "TERMINAL", "TermMode" },
+	["nt"] = { "TERMINAL", "TermMode" },
 }
 
 M.mode = function()
 	local m = vim.api.nvim_get_mode().mode
-	local current_mode = "%#" .. modes[m][2] .. "# " .. modes[m][1]
+	local current_mode = "%#" .. modes[m][2] .. "# " .. modes[m][1] .. " %#StatusLineFill#"
 	return current_mode
 end
 
@@ -48,21 +48,19 @@ M.lsp = function()
 	local ret = ""
 
 	local levels = {
-		['Error'] = ' ',
-		['Warn']  = ' ',
-		['Info']  = ' ',
-		['Hint']  = ' '
+		["Error"] = " ",
+		["Warn"] = " ",
+		["Info"] = " ",
+		["Hint"] = " ",
 	}
 
-	cmd('hi! link LspDiagnosticsWarn LspDiagnosticsWarning')
+	cmd("hi! link LspDiagnosticsWarn LspDiagnosticsWarning")
 	for level, ico in pairs(levels) do
 		local count
 		count = #vim.diagnostic.get(0, { severity = level })
 
-		ret = ret .. " "
-
 		if count ~= 0 then
-			ret = ret .. "%#LspDiagnostics" .. level .. "#" .. ico .. "%#Normal#" .. count
+			ret = ret .. "%#LspDiagnostics" .. level .. "#" .. ico .. "%#Normal#" .. count .. " "
 		end
 	end
 
